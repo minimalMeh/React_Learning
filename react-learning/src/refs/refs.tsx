cb5e639514232279aa7a20b488c13f9fe7da51ce
minimalMeh <dasha.osd@gmail.com>
2020-10-16 17:58:34 +0300

    add ref to component

--- react-learning/src/refs/refs.tsx
+++ react-learning/src/refs/refs.tsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 class SomeComponent extends Component {
+    private inputRef = React.createRef<HTMLInputElement>();
+
+    setValue() {
+        this.inputRef?.current?.focus();
+        const node = this.inputRef?.current;
+        if (node) {
+            node.value ="00";
+        }
+    }
+
     render() {
         return (
-            <input value="123" />
+            <input ref={this.inputRef} value="123" />
         );
     }
 }
 
 export default class RefSample extends Component {
-     textInput = React.createRef();
+    textInput = React.createRef();
+    componentTextInput = React.createRef();
+
 
     componentDidMount = () => {
         (this.textInput.current as any).value = 200;
-        // const inputValue = this._inputElement?.value; // в инпуте значение 456
-        // const someComp = ReactDOM.findDOMNode(this._someComp); //ссылка на инпут с 123
-        // const someCompValue = (someComp as HTMLInputElement)?.value; // 123
+        (this.componentTextInput.current as SomeComponent).setValue();
     }
 
     render() {
         return (
             <div>
                 <input value="456" ref={this.textInput as any} />
-                <SomeComponent />                    
+                <SomeComponent ref={this.componentTextInput as any}/>                    
             </div>
         );
     }
