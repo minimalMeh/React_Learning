18940ce86a4e855e7e3e75825b67a60fb836898a
minimalMeh <dasha.osd@gmail.com>
2020-10-17 18:43:28 +0300

    just did states

--- react-learning/src/App.tsx
+++ react-learning/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import RefSample from './refs/refs';
+import { Clock } from './states/states';
 
 function App() {
   return (
     <div className="App">
       <p>samples</p>
-      <RefSample/>
+      <RefSample />
+      <Clock />
     </div>
   );
 }
---
+++ react-learning/src/states/states.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export class Clock extends React.Component<{}, {date: Date}> {
+
+    timerID: NodeJS.Timeout;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {date: new Date()}; //начальное состояние в переменной this.state
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+        () => this.tick(),
+        2000
+        )
+    }
+  
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+    
+    render = () => {
+        return (
+            <div>
+                <h1>Time {this.state.date.toLocaleTimeString()}.</h1>
+            </div>
+        );
+    }
+}
+
